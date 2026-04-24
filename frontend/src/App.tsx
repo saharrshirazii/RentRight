cd1fe3615fde4a22c406cc8dd0de73888cbab194
saharrshirazii <saharr.shirazii@gmail.com>
2026-04-24 17:31:57 +0200

    I created 3 components and wrote server.js and db.js files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,18 @@
 import Price from "./components/Price/Price";
 import Subtitle from "./components/Subtitle/Subtitle";
 import Title from "./components/Title/Title";
+import { useState } from "react";
+import Dropdown from "./components/Dropdown/Dropdown";
+import Input from "./components/Input/Input";
+import Navbar from "./components/Navbar/Navbar";
+
+//Dropdow category
+const categoryOptions = [
+  { label: "Select All", value: "select_all" },
+  { label: "Electronics", value: "electronics" },
+  { label: "Clothing", value: "clothing" },
+  { label: "Home & Garden", value: "home_garden" },
+];
 
 function StatIcon({ icon, value }: { icon: string; value: number }) {
   return (
@@ -14,8 +26,17 @@ function StatIcon({ icon, value }: { icon: string; value: number }) {
 }
 
 export default function App() {
+  //dropdown select category
+  const [selectedCategory, setSelectedCategory] = useState<string>("Select a Category");
+
+  //handle select function to show an option when we select it.
+  const handleSelect = (option: { label: string; value: string | number }) => {
+    setSelectedCategory(option.label);
+  };
+
   return (
     <main className="page-shell">
+      <Navbar/>
       <section className="listing-card">
         <div className="listing-card__content">
           <Subtitle>Superhost i Stockholm</Subtitle>
@@ -30,6 +51,24 @@ export default function App() {
           </div>
         </div>
       </section>
+      <Dropdown
+        label={selectedCategory}
+        options={categoryOptions}
+        onSelect={handleSelect}
+      />
+
+      <Input
+        label="Email Address"
+        type="email"
+        placeholder="you@example.com"
+        onChange={(e) => console.log(e.target.value)}
+      />
+
+      <Input
+        label="Password"
+        type="password"
+        error="Password must be 8 characters" // Example of an error state
+      />
     </main>
   );
 }
