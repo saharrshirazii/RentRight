19db120236bffdb3667a73a07192204e5e6782bd
nm <nm.codes@hotmail.com>
2026-06-16 19:28:00 +0200

    Small fix

--- frontend/src/components/ProfilePropertyCard/ProfilePropertyCard.tsx
+++ frontend/src/components/ProfilePropertyCard/ProfilePropertyCard.tsx
@@ -24,9 +24,9 @@ const imageUrl = property.images?.[0]?.filename
       </Link>
 
       {/* Kortinnehåll */}
-      <div className="p-4 flex flex-col flex-grow">
+      <div className="p-4 flex flex-col grow">
         <div className="flex justify-between items-start">
-          <Link to={`/properties/${property._id}`} className="hover:underline block flex-grow max-w-[80%]">
+          <Link to={`/properties/${property._id}`} className="hover:underline block grow max-w-[80%]">
             <h3 className="font-bold text-gray-900 truncate">{property.title}</h3>
           </Link>
         
